589067a417759187f0da5c0e571f037dbfe67a4e
Jaspreet <Itsjaspreetk.aur7@gmail.com>
2023-06-27 15:22:45 -0400

    updated sysinfo for Lab 2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-#Lab-1
-echo 'FQDN:'
-hostname -f
+#echo commands are used to display labels
 
-echo 'Host Information:'
-hostnamectl
+#Hostname command to display information about fqdn
+fqdn=$(hostname -f)
 
-echo 'IP address:'
-hostname -I
+#Hostnamectl display OS info with awk command to only get required data
+hostname=$(hostnamectl | awk 'FNR == 7 {print $3, $4, $5}')
 
-echo 'Space Available:'
-df / -h
 
+ip=$(hostname -I)
+
+#df command runs along -h option for readable format to get data
+freespace=$(df / -h | awk 'FNR == 2 {print $4}')
+
+
+cat <<EOF
+                Report
+----------------------------------------
+FQDN: $fqdn
+Operating System name and version: $hostname
+IP Address: $ip
+Root Filesystem Free Space: $freespace
+----------------------------------------
+EOF
